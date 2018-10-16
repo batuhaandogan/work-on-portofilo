97d7221b2472d196da20f162169fb207c5d20d25
batuhaandogan <tipala_1909@hotmail.com>
2018-10-16 13:18:08 -0400

    navbar is perfect

--- starter-folder/src/App.js
+++ starter-folder/src/App.js
@@ -1,16 +1,40 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Button } from 'react-bootstrap';
+// import { Button } from 'react-bootstrap';
 
 
 class App extends Component {
   render() {
     return (
       <div>
-       
-    
 
-<Button bsStyle='success'>Hello</Button>
+
+       
+<navbar className="the-navbar-class">
+<ul className="main-nav">
+  <li >
+    <a href="/">Home</a>
+  </li>
+  <li >
+    <a href="/">Profile</a>
+  </li>
+  <li >
+    <a href="/">Contact</a>
+  </li>
+</ul>
+</navbar>
+
+
+
+<div className="main-image-first">
+
+<p id="the-p-tag">The significant problems<br/> we face cannot be solved by the same<br/> level of thinking that created them</p>
+<p id="the-second-p-tag">Turn your ideas into reality</p>
+</div>
+
+
+
+
 
 
 
