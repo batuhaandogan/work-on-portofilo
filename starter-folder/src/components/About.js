5c2ee7190d783211fcafe58167b7c07f7ae9ee66
batuhaandogan <tipala_1909@hotmail.com>
2018-10-17 12:04:56 -0400

    working on about pge

--- starter-folder/src/components/About.js
+++ starter-folder/src/components/About.js
@@ -9,8 +9,20 @@ class About extends Component {
       <div>
 
 
-     <div>
-       
+     <div className="about-me-page">
+
+     <h1>What's up, Dear Reader.<br/> I'm Tony Batuhan Dogan,<br/> a Junior Web Developer.</h1>
+      <h1 id="different-one">And this is about me page</h1>
+
+<div className="not-container">
+  <a data-scroll href="#full">
+    <div className="arrow"></div>
+  </a>
+</div>
+{/* <div id="full"></div> */}
+
+
+
      </div>
 
 
