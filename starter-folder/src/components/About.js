055429a9291197f876d13cd6f03807963de2e926
batuhaandogan <tipala_1909@hotmail.com>
2018-10-26 20:41:32 -0400

    about page

--- starter-folder/src/components/About.js
+++ starter-folder/src/components/About.js
@@ -13,8 +13,16 @@ class About extends Component {
     <link href="https://fonts.googleapis.com/css?family=Advent+Pro:100" rel="stylesheet" />
 </header>
 
+    <div className="for-personal-image">
+    <div className="for-the-opacity"></div>
+        <div className="my-image">
+        </div>
+      <h1>Tony Batuhan Dogan</h1>
+        </div>
 
-
+<div className="for-summary">
+      
+</div>
 
 
 
