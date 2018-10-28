b763f6b312f8b96b7348fb2dd048c3a756552859
batuhaandogan <tipala_1909@hotmail.com>
2018-10-28 12:47:20 -0400

    linkds

--- starter-folder/src/components/About.js
+++ starter-folder/src/components/About.js
@@ -11,6 +11,7 @@ class About extends Component {
 <header>
 
     <link href="https://fonts.googleapis.com/css?family=Advent+Pro:100" rel="stylesheet" />
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.2/css/all.css" integrity="sha384-/rXc/GQVaYpyDdyxK+ecHPVYJSN9bmVFBvjA/9eOB+pb3F2w2N6fc5qB9Ew5yIns" crossorigin="anonymous"/>
 </header>
 
 {/* <div className="all-background">
@@ -21,8 +22,38 @@ class About extends Component {
 <div className="circle-image">
 <h1>Tony Batuhan Dogan</h1>
 <p>Software Developer</p>
+
 </div>
+
+<p className="social-links">
+  <a href="https://www.linkedin.com/in/tonybatuhandogan/" target="_blank">
+  <span className="fab fa-linkedin the-icon" aria-hidden="true"></span>
+  </a>
+  <a href="https://www.linkedin.com/in/tonybatuhandogan/" target="_blank">
+  <span className="fab fa-github the-icon" aria-hidden="true"></span>
+  </a>
+  {/* <a href="https://www.linkedin.com/in/tonybatuhandogan/" target="_blank">
+  <span className="fab fa-linkedin the-icon" aria-hidden="true"></span>
+  </a> */}
+</p>
 </div>
+
+
+
+
+
+    <div className="project-down">
+        <h1 id="h1-tag">Main projects</h1>
+
+
+      <div>
+          <h1>Head Jump</h1>
+
+      </div>
+
+
+    </div>
+
 
 
 
